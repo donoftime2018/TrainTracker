584f7607693beffba79520b7811bcc295d815880
donoftime2018 <donoftime2018@gmail.com>
2023-11-17 20:17:13 -0500

    Frontend components done

--- vueapp/src/components/Dashboard.vue
+++ vueapp/src/components/Dashboard.vue
@@ -1,4 +1,5 @@
 <script setup>
+    import { ref } from 'vue';
 </script>
 
 <template>
--- vueapp/src/main.js
+++ vueapp/src/main.js
@@ -2,5 +2,10 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import Login from "./components/Login.vue"
+import Dashboard from "./components/Dashboard.vue"
+import ChangePassword from "./components/ChangePassword.vue"
+import Register from "./components/Register.vue"
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')

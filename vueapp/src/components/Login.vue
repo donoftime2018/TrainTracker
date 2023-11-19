b6a993dda7a1bffe87c5baf49339308197dda507
donoftime2018 <donoftime2018@gmail.com>
2023-11-19 18:17:04 -0500

    Form UI IP

--- vueapp/src/components/Login.vue
+++ vueapp/src/components/Login.vue
@@ -1,5 +1,26 @@
 <template>
-    <h1 class="text-blue-600 dark:text-blue-500">
-       Login
-    </h1>
+    <div class="flex justify-center">
+        <div class="mt-20 w-full max-w-xl">
+            <form class="bg-white shadow-md rounded px-12 pt-6 pb-8 mb-4">
+                    <div class="mb-4">
+                        <h2 class="text-center font-bold text-2xl">Login</h2>
+                    </div>
+                 
+                    <div class="mb-4">
+                        <label class="text-center block text-gray-700 text-sm font-bold mb-2" for="username">
+                            Username
+                        </label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+                    </div>
+
+                    <div class="mb-4">
+                        <label class="text-center block text-gray-700 text-sm font-bold mb-2" for="password">
+                            Password
+                        </label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password">
+                    </div>
+            </form>
+        </div>
+    </div>
+    
 </template>

d76d8837929f6e5851c5af1209e2fdcd3af0bd48
donoftime2018 <donoftime2018@gmail.com>
2023-11-19 20:27:11 -0500

    login form done

--- vueapp/src/components/Login.vue
+++ vueapp/src/components/Login.vue
@@ -1,24 +1,34 @@
+<script setup>
+    
+</script>
+
 <template>
     <div class="flex justify-center">
         <div class="mt-20 w-full max-w-xl">
-            <form class="bg-white shadow-md rounded px-12 pt-6 pb-8 mb-4">
-                    <div class="mb-4">
-                        <h2 class="text-center font-bold text-2xl">Login</h2>
-                    </div>
-                 
-                    <div class="mb-4">
-                        <label class="text-center block text-gray-700 text-sm font-bold mb-2" for="username">
-                            Username
-                        </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
-                    </div>
+            <form class="bg-[#EE0034] text-white shadow-md rounded px-12 pt-6 pb-8 mb-4">
+                <div class="mb-4">
+                    <h1 class="text-center font-bold text-2xl underline">Login</h1>
+                </div>
 
-                    <div class="mb-4">
-                        <label class="text-center block text-gray-700 text-sm font-bold mb-2" for="password">
-                            Password
-                        </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password">
-                    </div>
+                <div class="mb-4">
+                    <label class="text-center block text-lg font-bold mb-2" for="username">
+                        Username
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username goes here...">
+                </div>
+
+                <div class="mb-4">
+                    <label class="text-center block text-lg font-bold mb-2" for="password">
+                        Password
+                    </label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password goes here...">
+                </div>
+
+                <div class="text-center flex flex-col">
+                    <a href="/register" onclick="">Need to register?</a>
+                    <a href="/changePassword">Forgot password?</a>
+                </div>
+              
             </form>
         </div>
     </div>

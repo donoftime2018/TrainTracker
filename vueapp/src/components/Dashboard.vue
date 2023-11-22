972042ad89494cc1a28ce2fc56667f1ee00f04fc
donoftime2018 <donoftime2018@gmail.com>
2023-11-21 21:59:35 -0500

    API data read successfully

--- vueapp/src/components/Dashboard.vue
+++ vueapp/src/components/Dashboard.vue
@@ -1,5 +1,26 @@
 <template>
-   <h1 class="text-blue-600 dark:text-blue-500">
-      Dashboard
-    </h1>
+  <div>
+      {{trainData}}
+  </div>
 </template>
+
+<script>
+    import { onMounted } from "vue";
+    import axios from "axios";
+
+    export default {
+        data() {
+            return {
+                trainData: null
+            };
+        },
+        mounted() {
+            console.log("Mounted!")
+
+            axios.get("http://localhost:5118/api/getJsonData").then(response => {
+                console.log(response);
+                this.trainData = response.data
+            })
+        }
+    }
+</script>
---
+++ webapi/Controllers/ApiController.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Xml;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers
+{
+    [Route("api/getJsonData")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]      
+        public async Task<ActionResult<String>> GetData()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage res = await client.GetAsync("https://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=f3668ac334484bc88b6b2e7778a013bd&mapid=40380");
+
+                if (res.IsSuccessStatusCode)
+                {
+                    string XMLData = await res.Content.ReadAsStringAsync();
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.LoadXml(XMLData);
+                    string jsonData = JsonConvert.SerializeXmlNode(xmlDoc);
+                    return Ok(jsonData);
+                }
+
+                else
+                {
+                    return StatusCode((int)res.StatusCode, "Error fetching XML data");
+                }
+            }
+                
+            }
+
+
+        }
+    }

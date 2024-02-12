782f48ba01f6be583e9c44628b2572370a65a862
donoftime2018 <donoftime2018@gmail.com>
2024-02-11 21:06:35 -0500

    Make CI/CD work + go to station on google maps

--- vueapp/src/components/Dashboard.vue
+++ vueapp/src/components/Dashboard.vue
@@ -6,23 +6,26 @@
         </div>
     </div>
 
-        <div v-if="errMessage !== '' && (trainData === undefined || trainData === [])" class="flex justify-evenly mt-40 text-3xl"><h1>{{errMessage}}</h1></div>
+        <div v-if="errMessage !== '' && (trainData === undefined || trainData === [])" class="flex shadow-xl justify-evenly mt-40 text-3xl"><h1>{{errMessage}}</h1></div>
 
         <div v-else class="flex flex-wrap gap-1 justify-evenly">
             
-            <div v-for="t in trainData" class="mt-20 text-center text-white px-6 py-4 divide-y">
-                <div class="flex bg-[#c60c30] p-3 w-full max-w-xl">
-                    <div class="flex flex-col"><h2 class="font-bold underline">Parent Station:</h2> <p>{{t.staNm}}</p></div>
-                    <div class="flex flex-col"><h2 class="font-bold underline">Station Description:</h2> <p>{{t.stpDe}}</p></div>
-                    <div class="flex flex-col"><h2 class="font-bold underline">Destination Station:</h2> <p>{{t.destNm}}</p></div>
-                    <div class="flex flex-col"><h2 class="font-bold underline">Arrival Time:</h2> <p>{{t.arrT}}</p></div>
+            <div v-for="t in trainData" class="mt-20 rounded-lg text-center text-white px-6 py-4">
+                <div class="shadow-xl divide-y">
+                    <div class="flex bg-[#c60c30] p-3 w-full max-w-xl">
+                        <div class="flex flex-col"><h2 class="font-bold underline">Parent Station:</h2> <p v-bind:style="{cursor: pointer}" @click="googleMapStation(t.staNm, t.stpDe)">{{t.staNm}}</p></div>
+                        <div class="flex flex-col"><h2 class="font-bold underline">Station Description:</h2> <p>{{t.stpDe}}</p></div>
+                        <div class="flex flex-col"><h2 class="font-bold underline">Destination Station:</h2> <p v-bind:style="{cursor: pointer}" @click="googleMapStation(t.destNm, t.stpDe)">{{t.destNm}}</p></div>
+                        <div class="flex flex-col"><h2 class="font-bold underline">Arrival Time:</h2> <p>{{t.arrT}}</p></div>
 
+                    </div>
+                    <div class="flex justify-evenly bg-[#c60c30] mb-5 p-3 w-full max-w-xl">
+                        <div class="flex flex-col"><h2 class="font-bold underline">Approaching:</h2><p>{{t.isApp === "1" ? 'Yes' : 'No'}}</p></div>
+                        <div class="flex flex-col"><h2 class="font-bold underline">Delayed:</h2><p>{{t.isDly === "1" ? 'Yes' : 'No'}}</p></div>
+                        <div class="flex flex-col"><h2 class="font-bold underline">Faulty Data:</h2><p>{{t.isFlt === "1" ? 'Yes' : 'No'}}</p></div>
+                    </div>
                 </div>
-                <div class="flex justify-evenly bg-[#c60c30] mb-5 p-3 w-full max-w-xl">
-                    <div class="flex flex-col"><h2 class="font-bold underline">Approaching:</h2><p>{{t.isApp === "1" ? 'Yes' : 'No'}}</p></div>
-                    <div class="flex flex-col"><h2 class="font-bold underline">Delayed:</h2><p>{{t.isDly === "1" ? 'Yes' : 'No'}}</p></div>
-                    <div class="flex flex-col"><h2 class="font-bold underline">Faulty Data:</h2><p>{{t.isFlt === "1" ? 'Yes' : 'No'}}</p></div>
-                </div>
+            
             </div>
 
         </div>
@@ -34,13 +37,21 @@
     import { onMounted, ref } from 'vue';
 
     export default {
-       
+
         data() {
             return {
                 trainData: [],
                 currentTime: null,
                 errMessage: null
             };
+        },
+        methods: {
+            googleMapStation(stationName) {
+
+                let urlParams = stationName + " station, Illinois"
+
+                window.open("https://www.google.com/maps/search/"+urlParams)
+            }
         },
         async mounted() {
             console.log("Mounted!")
